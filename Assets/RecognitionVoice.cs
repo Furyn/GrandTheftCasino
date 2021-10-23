fd19cf29d4eac7a331ee2986e5cb00f0306dc10a
Tobias <Tobias.orange@gmail.com>
2021-10-23 17:52:35 +0200

    systeme de reconnaissance vocal avec scene test

---
+++ Assets/RecognitionVoice.cs
@@ -0,0 +1,109 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+
+public class RecognitionVoice : MonoBehaviour
+{
+    protected DictationRecognizer dictationRecognizer;
+    private string[] tableauString;
+    private
+    void Start()
+    {
+        StartDictationEngine();
+    }
+    private void DictationRecognizer_OnDictationHypothesis(string text)
+    {
+        Debug.Log("Dictation hypothesis: " + text);
+    }
+    private void Update()
+    {
+
+    }
+
+    private void DictationRecognizer_OnDictationComplete(DictationCompletionCause completionCause)
+    {
+        switch (completionCause)
+        {
+            case DictationCompletionCause.TimeoutExceeded:
+            case DictationCompletionCause.PauseLimitExceeded:
+            case DictationCompletionCause.Canceled:
+            case DictationCompletionCause.Complete:
+                // Restart required
+                CloseDictationEngine();
+                StartDictationEngine();
+                break;
+            case DictationCompletionCause.UnknownError:
+            case DictationCompletionCause.AudioQualityFailure:
+            case DictationCompletionCause.MicrophoneUnavailable:
+            case DictationCompletionCause.NetworkFailure:
+                // Error
+                CloseDictationEngine();
+                break;
+        }
+    }
+    private void DictationRecognizer_OnDictationResult(string text, ConfidenceLevel confidence)
+    {
+        tableauString = text.Split(' ');
+        foreach (string sub in tableauString)
+        {
+            if (sub == "haut")
+            {
+
+                transform.Translate(0, 1, 0);
+                Debug.Log("tu as dis haut");
+            }
+
+            if (sub == "bas")
+            {
+                transform.Translate(0, -1, 0);
+                Debug.Log("tu as dis bas");
+            }
+            if (sub == "gauche")
+            {
+                transform.Translate(-1, 0, 0);
+                Debug.Log("tu as dis gauche");
+            }
+            if (sub == "droite")
+            {
+                transform.Translate(1, 0, 0);
+                Debug.Log("tu as dis droite");
+            }
+           Debug.Log($"Substring: {sub}");
+        }
+        Debug.Log("Dictation result: " + text);
+    }
+
+    private void DictationRecognizer_OnDictationError(string error, int hresult)
+    {
+        Debug.Log("Dictation error: " + error);
+    }
+    private void OnApplicationQuit()
+    {
+        CloseDictationEngine();
+    }
+    private void StartDictationEngine()
+    {
+        dictationRecognizer = new DictationRecognizer();
+        dictationRecognizer.DictationHypothesis += DictationRecognizer_OnDictationHypothesis;
+        dictationRecognizer.DictationResult += DictationRecognizer_OnDictationResult;
+        dictationRecognizer.DictationComplete += DictationRecognizer_OnDictationComplete;
+        dictationRecognizer.DictationError += DictationRecognizer_OnDictationError;
+        dictationRecognizer.Start();
+    }
+    private void CloseDictationEngine()
+    {
+        if (dictationRecognizer != null)
+        {
+            dictationRecognizer.DictationHypothesis -= DictationRecognizer_OnDictationHypothesis;
+            dictationRecognizer.DictationComplete -= DictationRecognizer_OnDictationComplete;
+            dictationRecognizer.DictationResult -= DictationRecognizer_OnDictationResult;
+            dictationRecognizer.DictationError -= DictationRecognizer_OnDictationError;
+            if (dictationRecognizer.Status == SpeechSystemStatus.Running)
+            {
+                dictationRecognizer.Stop();
+            }
+            dictationRecognizer.Dispose();
+        }
+    }
+}
