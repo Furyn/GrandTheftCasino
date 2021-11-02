fa2abd47ba83622f1fc39d9e1787e7193f229655
Furyn <vincent_1999@hotmail.fr>
2021-11-02 08:39:31 +0100

    [ADD] Guard go to phone and wait

--- Assets/Scripts/GuardController.cs
+++ Assets/Scripts/GuardController.cs
@@ -7,10 +7,22 @@ public class GuardController : MonoBehaviour
 {
     public Transform[] passPoint;
     public NavMeshAgent agent;
+    public float distance_trigger_phone = 5;
+    public float wait_on_phone = 2f;
+
+    private bool onPhone = false;
+    private float timer_on_phone = 0f;
     private int index = 0;
+    private PhoneManager phoneManager = null;
 
     void Start()
     {
+        phoneManager = FindObjectOfType<PhoneManager>();
+        if (phoneManager == null)
+        {
+            Debug.LogError("No phone manager found");
+        }
+
         if (passPoint.Length > index)
         {
             agent.SetDestination(passPoint[index].position);
@@ -20,15 +32,37 @@ public class GuardController : MonoBehaviour
 
     void Update()
     {
+        if (onPhone)
+        {
+            timer_on_phone -= Time.deltaTime;
+            if (timer_on_phone >= 0f)
+            {
+                onPhone = false;
+                timer_on_phone = 0f;
+                agent.SetDestination(passPoint[index].position);
+            }
+        }
+        else
+        {
+            foreach (PhoneManager.PhoneInfo phone in phoneManager.spawnPhoneInfo)
+            {
+                float distance = Vector3.Distance(phone.PhoneObject.transform.position, agent.gameObject.transform.position);
+                if (distance_trigger_phone > distance) // && phone.isDringDring
+                {
+                    agent.SetDestination(phone.PhoneObject.transform.position);
+                    timer_on_phone = wait_on_phone;
+                    onPhone = true;
+                }
+            }
+        }
 
-        if (!agent.pathPending)
+        if (!agent.pathPending && !onPhone)
         {
             if (agent.remainingDistance <= agent.stoppingDistance)
             {
                 if (!agent.hasPath || agent.velocity.sqrMagnitude == 0f)
                 {
                     index++;
-                    Debug.Log("HERE");
                     if (passPoint.Length < index)
                     {
                         index = 0;
@@ -41,11 +75,5 @@ public class GuardController : MonoBehaviour
             }
         }
 
-            
-            /*
-            */
-
-            
-        
     }
 }
