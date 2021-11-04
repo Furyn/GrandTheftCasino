2aed1d7e28d52913ffb3d4ea65df00af2a9111d1
Nolan <nolan.jutard@gmail.com>
2021-11-04 13:47:44 +0100

    Urp + Glow (modif shader selection plants + glass)

---
+++ Assets/Materials/Glow/SetOutline.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class SetOutline : MonoBehaviour
+{
+    [SerializeField] private Material outlineMaterial;
+    [SerializeField] private float outlineScaleFactor;
+    private Color outlineColor;
+    private Renderer outlineRenderer;
+
+    void Start()
+    {
+        outlineMaterial = (Material)AssetDatabase.LoadAssetAtPath("Assets/Materials/Glow/outlineMat.mat", typeof(Material));
+        outlineRenderer = CreateOutline(outlineMaterial, outlineScaleFactor, outlineColor);
+    }
+
+    Renderer CreateOutline(Material outlineMat, float scaleFactor, Color color)
+    {
+
+        GameObject outlineObject = Instantiate(this.gameObject, transform.position, transform.rotation, transform);
+        outlineObject.transform.localScale = new Vector3(1, 1, 1);
+        Renderer rend = outlineObject.GetComponent<Renderer>();
+        rend.material = outlineMat;
+        rend.material.SetColor("_OutlineColor", color);
+        rend.material.SetFloat("_ScaleFactor", scaleFactor);
+        rend.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;
+        outlineObject.GetComponent<SetOutline>().enabled = false;
+        if (outlineObject.GetComponent<Collider>())
+        {
+            outlineObject.GetComponent<Collider>().enabled = false;
+        }
+        rend.enabled = false;
+
+        return rend;
+    }
+    private void Update()
+    {
+        outlineRenderer.enabled = true;
+    }
+
+    //private void OnMouseEnter()
+    //{
+    //    outlineRenderer.enabled = true;
+    //}
+
+    //private void OnMouseOver()
+    //{
+    //    transform.Rotate(Vector3.up, 1f, Space.World);
+    //}
+
+    //private void OnMouseExit()
+    //{
+    //    outlineRenderer.enabled = false;
+    //}
+}
