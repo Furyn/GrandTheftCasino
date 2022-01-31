7f96232c5642201304a62b5b8bf0c846725ec76f
Furyn <vincent_1999@hotmail.fr>
2022-01-31 21:56:40 +0100

    [FIX] Launch first scene with 1 on phone

--- Assets/SceneLaunching.cs
+++ Assets/SceneLaunching.cs
@@ -5,11 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class SceneLaunching : MonoBehaviour
 {
+    public Arduino_test arduino = null;
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Keypad1) || Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Keypad1) || Input.GetKeyDown(KeyCode.Space) || arduino.data == "1")
         {
             SceneManager.LoadScene("Baptiste LD");
         }
